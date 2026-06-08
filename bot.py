06abafa521f2584d872498fb24886d0ff0b20dc0
calgoneq <calgoneq@gmail.com>
2026-06-08 19:48:38 +0200

    feat: integrate telegram api

---
+++ bot.py
@@ -0,0 +1,46 @@
+import asyncio
+from telegram import Update
+from telegram.ext import ContextTypes, Application, CommandHandler, MessageHandler, filters
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+from content_generation import get_content
+from ai import generate_summary
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hi! Send me the link to your article and I will summarize it for you!")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        url = update.message.text
+        parsed = urlparse(url)
+    
+        if not parsed.scheme or not parsed.netloc:
+            await update.message.reply_text("Send me only the link to the article, not a text!")
+            return
+        
+        await update.message.reply_chat_action(action="typing")
+
+        content = await get_content(url)
+        if "Error: " in content:
+            await update.message.reply_text("There was some error during content generation, please try again!")
+            return
+        summary = await generate_summary(content)
+        await update.message.reply_text(summary, parse_mode="Markdown")
+    except Exception as e:
+        await update.message.reply_text("There was an unexpected error! Sorry for the issue, try again later.")
+        print(f"Error: {e}")
+        return
+
+def main():
+    """Start the bot."""
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import asyncio
-
-from content_generation import get_content
-from ai import generate_summary
+from bot import main as bot_main
 
 def main():
-    url = "https://www.artificialintelligence-news.com/"
-    url_quotes="https://quotes.toscrape.com/"
-    url_exception = "https://www.artificialintelligence-news.com/news/microsofts-autopilot-scout-is-the-agentic-autopilot-that-works-across-m365/"
-    data = asyncio.run(get_content(url_quotes))
-    ai_summary = asyncio.run(generate_summary(data))
-    print(ai_summary)
+    bot_main()
 
 if __name__ == "__main__":
     main()
